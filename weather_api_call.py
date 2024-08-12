abfa7b340f81c771c94b88c00a09e4660ffff179
dpatel4603 <51136648+dpatel4603@users.noreply.github.com>
2024-08-12 15:48:27 -0500

    version --2 push by Dev Patel

--- weather_api_call.py
+++ weather_api_call.py
@@ -1,41 +1,56 @@
+import sys
+
 import requests
 from geopy.geocoders import Nominatim
+import datetime
 
 geolocator = Nominatim(user_agent="weather_api_call")
+date = datetime.date.today()
 
 
 def get_location():
     location_confirmation = "no"
     while location_confirmation == "no":
-        location_call = input("What is the address of where you want the weather forecast?\n")
-        location = geolocator.geocode(location_call)
-        print(location.address)
-        location_confirmation = input("Is the following address correct? type yes or no: ")
-        location_confirmation.lower()
-        latitude = location.latitude
-        longitude = location.longitude
+        try:
+            location_call = input("What is the American address of where you want the weather forecast?\n")
+            location = geolocator.geocode(location_call)
+            print(location.address)
+            location_confirmation = input("Is the following address correct? type yes or no: ")
+            location_confirmation.lower()
+            latitude = location.latitude
+            longitude = location.longitude
 
-    return latitude, longitude
+        except AttributeError:
+            print("INVALID ADDRESS")
+            break
+
+        return latitude, longitude
 
 
 def get_weather(latitude, longitude):
     response = requests.get(f"https://api.weather.gov/points/{latitude},{longitude}")
     data = response.json()
 
-    # Step 2: Extract the forecast URL
-    forecast_url = data['properties']['forecast']
+    how_many_days = input("How many days do you want to find the weather for, limit is one week?\n")
 
+    # Step 2: Extract the forecast URL
+    try:
+        forecast_url = data['properties']['forecast']
+
+    except KeyError:
+        print("INVALID FORECAST")
+        sys.exit()
     # Step 3: Get the detailed forecast
     forecast_response = requests.get(forecast_url)
     forecast_data = forecast_response.json()
-    return (f"{forecast_data['properties']['periods'][1]['name']} the weather will be "
-            f"{forecast_data['properties']['periods'][1]['detailedForecast']}")
+    for i in range(int(how_many_days) * 2):
+        print(f"{forecast_data['properties']['periods'][i]['name']} the weather will be "
+              f"{forecast_data['properties']['periods'][i]['detailedForecast']}")
 
 
 def main():
     latitude, longitude = get_location()
-    weather = get_weather(latitude, longitude)
-    print(weather)
+    get_weather(latitude, longitude)
 
 
 if __name__ == "__main__":
